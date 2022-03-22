49049df6f6dd3284602d9607bf0c4b8de1d596a5
Meltem Candan <meltemcandan52@gmail.com>
2022-03-22 20:47:31 +0300

    İf-Else Örneği

---
+++ Homework2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NotEverage();
+        }
+
+        public static void NotEverage()
+        {
+            double vize, final, sonuc;
+            Console.Write("Vize Notunuzu Giriniz: ");
+            vize = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Final Notunuzu Giriniz: ");
+            final = Convert.ToDouble(Console.ReadLine());
+
+            sonuc = vize * 0.4 + final * 0.6;
+            if (sonuc > 60)
+            {
+                Console.WriteLine("Başarılı");
+            }
+            else
+            {
+                Console.WriteLine("Başarısız");
+            }
+            Console.ReadLine();
+        }
+    }
+}
