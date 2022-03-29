48dddc59fe8ad7783f91044808112a293fe8c728
Meltem Candan <meltemcandan52@gmail.com>
2022-03-29 20:35:55 +0300

    İç içe for döngüsü kullanım örneği

---
+++ Homework7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //İç içe for döngüsü kullanım örneği
+            int sayi = 10;
+            for (int i = 1; i <= sayi; i++)
+            {
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write(k);
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
