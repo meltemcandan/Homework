5ce94bfb79c1e187850a7bc95278d6de7ae8887c
Meltem Candan <meltemcandan52@gmail.com>
2022-03-29 21:18:42 +0300

    Do While döngü kullanımı örneği

---
+++ Homework9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1' den 100 ' e kadar olan sayıların toplamını yazdırma
+            int a = 1;
+            int toplam = 0;
+            do
+            {
+                toplam += a;
+                a++;
+            }
+
+            while (a <= 100);
+            Console.WriteLine("1' den 100' e kadar olan sayıların toplamı: " + toplam);
+            Console.ReadLine();
+        }
+    }
+}
