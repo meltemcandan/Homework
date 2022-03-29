412a0975a578939fc2b5de24f7ac1a9b310f2611
Meltem Candan <meltemcandan52@gmail.com>
2022-03-29 21:51:11 +0300

    İç içe for döngüsüyle çarpım tablosu örneği

---
+++ Homework10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Çarpım tablosu örneği
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 0; j <= 10; j++)
+                {
+                    Console.WriteLine(i + "x" + j + "=" + (i * j));
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
