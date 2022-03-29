ca5701784f9c00618f899cd9b7d597834cec8c29
Meltem Candan <meltemcandan52@gmail.com>
2022-03-29 20:50:41 +0300

    While döngü örneği

---
+++ Homework8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 100' den 0'a kadar olan çift sayıları yazdırma örneği
+            int sayi = 100;
+            while (sayi >= 0)
+            {
+                Console.WriteLine(sayi);
+                sayi -= 2;
+            }
+            Console.ReadLine();
+        }
+    }
+}
