3b071fc64ceba27bca1059bdcdc45449ffb80504
Meltem Candan <meltemcandan52@gmail.com>
2022-04-01 15:36:58 +0300

    Metot Oluşturma Örneği

---
+++ Homework14/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int DonenDeger = toplamaYap (6, 7);
+            Console.WriteLine(DonenDeger);
+            Console.ReadLine();
+            
+        }
+        static int toplamaYap(int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+
+            return sonuc;
+
+        }
+    }
+}
