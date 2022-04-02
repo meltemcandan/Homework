d0eee0c5955f7b7a4b9b34ef65b127246c9517cb
Meltem Candan <meltemcandan52@gmail.com>
2022-04-02 20:44:06 +0300

    Diziler Örneği

---
+++ Homework15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Haftanın günlerini bir dizi içine atayarak ekrana yazdırma örneği
+
+            string[] days = { "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi", "Pazar" };
+
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(days[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
