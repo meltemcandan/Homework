495a4fbd7979f984f1d7177a97d8d5a9474f0d48
Meltem Candan <meltemcandan52@gmail.com>
2022-04-03 23:15:21 +0300

    Method Kullanarak Oluşturulan Araba Örneği

---
+++ Homework19/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework19
+{
+    public class Car
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public string Rengi { get; set; }
+        public int Yili { get; set; }
+        public int KmBasiYakit { get; set; }
+
+        public Car(string _Marka, string _Model, string _Rengi, int _Yili, int _KmBasiYakit)
+        {
+            Marka = _Marka;
+            Model = _Model; 
+            Rengi = _Rengi; 
+            Yili = _Yili;   
+            KmBasiYakit = _KmBasiYakit;
+                    
+        }
+        public string CarData()
+        {
+           return "Araba Markası: " + Marka + "\nAraba Modeli: " + Model + "\nAraba Rengi: " + Rengi + "\nAraç Yapım Yılı: " + Yili + "\nAraç Km Başı Harcadığı Yakıt: " + KmBasiYakit;
+        }
+
+        public int YakitHesaplama(int gidecegiKm)
+        {
+            return (gidecegiKm * KmBasiYakit);
+           
+        }
+    }
+
+}
---
+++ Homework19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Homework19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Hyundai", "Bayon", "Gri", 2021, 3);
+
+            string CarData = car1.CarData();
+            Console.WriteLine("Araba Bilgileri:\n\n" + CarData);
+            Console.WriteLine();
+            Console.WriteLine("Lütfen Km Giriniz: ");
+            int Km=Convert.ToInt32(Console.ReadLine());
+            int yakitHesaplama =car1.YakitHesaplama(Km);
+            Console.WriteLine("Belirttiğiniz km'ye göre araç toplam " + yakitHesaplama + "TL yakıt arcamaktadır.");
+            Console.ReadLine();
+        }
+    }
+}
