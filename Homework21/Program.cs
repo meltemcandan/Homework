f4092610cf20e4b89e21f843a2835a7529b00b32
Meltem Candan <meltemcandan52@gmail.com>
2022-04-05 13:40:43 +0300

    Girilen bir cümledeki kelime sayısını bulan program

---
+++ Homework21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homework21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen bir sayının asal olduğunu bulan ve bu sayıdan sonraki 5 asal sayıyı ekrana yazan program
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            double sayi= Convert.ToDouble(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Homework22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Girilen bir cümledeki kelime sayısını bulunuz
+            Console.WriteLine("Lütfen bir cümle yazınız: ");
+            string cümle= Console.ReadLine();
+            string[] kelimeler = cümle.Split(' ');
+            Console.WriteLine(kelimeler.Length);
+            Console.ReadLine();
+        }
+    }
+}
