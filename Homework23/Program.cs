448b68029e35978fc1a62ceeb38c915848379ffd
Meltem Candan <meltemcandan52@gmail.com>
2022-04-05 20:55:18 +0300

    Komisyon değeri hesaplayan program

---
+++ Homework23/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Homework23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bir otoparka park eden taksinin 1 saati 5TL, minibüsün 1 saati 6TL, ticari aracın 1 saati 6.5TL dir. Taksi 1 saatten sonraki her saat başı için %20 daha fazla, minibüs 1 saatten sonraki her saat başı için toplamda %21.5 ve ticari araç 1 saatten sonraki her saat başı için toplamda %25 daha fazla ödeme yapmaktadır. Buna göre  girilen araba tipi ve kalınan saat bilgisi girildikten sonra ekrana ödenecek otopark ücretini hesaplayan programın kodlarını yazınız.
+
+            double odenecekTutar;
+            int kalinanSure=0;
+            int aracTipi = 0;
+            
+            Console.WriteLine("Taksi: 1");
+            Console.WriteLine("Minibüs: 2");
+            Console.WriteLine("Ticari Araç: 3");
+            Console.WriteLine("Lütfen araç tipini seçiniz: ");
+            aracTipi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Lütfen kalınan süreyi giriniz: ");
+            kalinanSure = Convert.ToInt32(Console.ReadLine());
+
+            if (kalinanSure>1)
+            {
+                for (double i = 1; i <= kalinanSure; i++)
+                {
+                    if (i==1)
+                    {
+                        if (aracTipi==1)
+                        {
+                            Console.WriteLine(odenecekTutar = (i*5));
+                        }
+                        else if (aracTipi==2)
+                        {
+                            Console.WriteLine(odenecekTutar = (i * 6));
+                        }
+                        else if (aracTipi==3)
+                        {
+                            Console.WriteLine(odenecekTutar = (i * 6.5));
+                        }
+
+                    }
+                    else
+                    {
+                        kalinanSure-=1;
+                        
+
+                        if (aracTipi == 1)
+                            
+                        {
+                            Console.WriteLine( odenecekTutar += kalinanSure * 1.20 * 5);
+                        }
+                        else if (aracTipi == 2)
+                        {
+                            Console.WriteLine(odenecekTutar += kalinanSure * 6 * 1.215 );
+                        }
+                        else if (aracTipi == 3)
+                        {
+                            Console.WriteLine(odenecekTutar += kalinanSure* 6.5 * 1.25);
+                        }
+
+                    }
+                }
+                
+                               
+            }
+            else
+            {
+                if (aracTipi == 1)
+                {
+                    Console.WriteLine(odenecekTutar =  5);
+                }
+                else if (aracTipi == 2)
+                {
+                    Console.WriteLine(odenecekTutar = 6);
+                }
+                else if (aracTipi == 3)
+                {
+                    Console.WriteLine(odenecekTutar = 6.5);
+                }
+            }
+
+            Console.WriteLine("Ödenecek Tutar: " + odenecekTutar);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Homework24/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bir komisyoncu sattığı mallardan fiyatı 50 TL kadar olanlardan % 3, daha fazla olanlardan ise % 2 komisyon almaktadır. Klavyeden girilen teker teker girilen 5 malın komisyonlarını bulup ekrana yazdıran ve en sonunda da toplam komisyonu ekrana yazdıran programın kodlarını yazınız.
+
+            double malFiyati = 0, komisyonMiktari = 0, toplamKomisyon = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("{0}. Malın Fiyatını Giriniz : ", i + 1);
+                malFiyati = Convert.ToDouble(Console.ReadLine());
+                if (malFiyati > 50)
+                { komisyonMiktari = malFiyati * 0.02; }
+                else
+                { komisyonMiktari = malFiyati * 0.03; }
+                Console.WriteLine("{0}. Mal İçin Komisyon Miktarı : {1}", i + 1, komisyonMiktari);
+                toplamKomisyon += komisyonMiktari;
+            }
+            Console.WriteLine("Toplam Komisyon Miktarı : {0}", toplamKomisyon);
+
+            Console.ReadLine();
+
+        }
+    }
+}
