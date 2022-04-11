bce332dec84d161a3be1a79dc49e00df99d40f6d
Meltem Candan <meltemcandan52@gmail.com>
2022-04-11 20:12:01 +0300

    Verilen bir karakter dizininin substring() metodunu kullanarak string içerisinde arama yapan ve kaç defa geçtiğini bulan program

---
+++ Homework32/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Homework32
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Verilen bir karakter dizininin substring() metodunu kullanarak string içerisinde arama yapan ve kaç defa geçtiğini bulan program
+            int sayac = 0;
+            string metin = "mevsimler: Kış, İlkbahar, Yaz, Sonbahar";
+            string aranan = "Yaz";
+            int metinuzunluk = metin.Length;
+            int arananuzunluk = aranan.Length;
+
+            for (int i = 0; i <= (metinuzunluk - arananuzunluk); i++)
+            {
+                if (metin.Substring(i, arananuzunluk) == aranan)
+                {
+                    sayac++;
+                }
+            }
+            Console.WriteLine(sayac);
+            Console.ReadKey();
+        }
+    }
+}
